1411a60ede64a11075521c9c973b413cb2f32d53
Brian Walsh <bwwalshjr@gmail.com>
2017-09-22 14:12:36 -0400

    Updating docs

--- basicfunctions.py
+++ basicfunctions.py
@@ -8,3 +8,31 @@ def add2(x,y):
 def myRange(start, end, increment=1):
 	return range(start, end, increment)
 	#Homework: rewrite this function to use a for loop
+	rng = [start]
+	start += 1
+	while (start<end):
+		rng.add(start)
+		start += increment
+	return rng
+	
+#Prints a triangle with side length and height n
+def print_triangle(n, full=False):
+	line_number = 1
+	while(line_number <= n):
+		print('*' * line_number)
+		line_number += 1
+		
+	if(full):
+		line_number -= 2
+		while(line_number >= 1):
+			print('*' * line_number)
+			line_number -= 1
+			
+#Returns a histogram with item counts
+def histogram(items):
+	d = {}
+	for item in items:
+		if not(d.has_key(item)):
+			d[item] = 0
+		d[item] += 1
+	return d
--- testBs.py
+++ testBs.py
@@ -4,11 +4,24 @@
 import basicfunctions as bs
 
 #Test the add2 functions
-print(bs.add2(3,5))
-print(bs.add2(4,6))
-print(bs.add2(5,7))
+#print(bs.add2(3,5))
+#print(bs.add2(4,6))
+#print(bs.add2(5,7))
 
 #Testing range function
-print(bs.myRange(1,50))
-print(bs.myRange(1,50,3))
-print(bs.myRange(1, 50, increment = 4)) #Note that we can specify the optional argument inputs
+#print("My range: " + str(bs.myRange(1,50)))
+#print("Py range: " + str(range(1,50)))
+#print("My range: " + str(bs.myRange(1,50,3)))
+#print("Py range: " + str(range(1,50,3)))
+#print("My range: " + str(bs.myRange(1, 50, increment = 4))) #Note that we can specify the optional argument inputs
+#print("Py range: " + str(range(1,50,4)))
+
+#Test the triangle printing function
+#bs.print_triangle(3)
+#print("\n")
+#bs.print_triangle(5)
+#print("\n")
+#bs.print_triangle(5,full=True)
+
+print(bs.histogram(['a','x', 2, 'x', 3, 2]))
+print(bs.histogram(['1','2','2','3','3','3','a','b','c','c']))
